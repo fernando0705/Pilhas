8e8b54437807f671c56b307e56c59b3cfa797160
Fernando <fernandofilho0705@gmail.com>
2026-03-23 17:37:43 -0300

    Removendo arquivos

--- src/MainListaDinamica.java
+++
@@ -1,9 +0,0 @@
-public class MainListaDinamica {
-    public static void main(String[] args) {
-        ListaDinamica listaDinamica = new ListaDinamica();
-        listaDinamica.adicionarElemento("Arroz");
-        listaDinamica.adicionarElemento("Feijão");
-        listaDinamica.removerElemento("Feijão");
-        listaDinamica.exibirElementos();
-    }
-}
